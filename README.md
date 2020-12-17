ef08d7d37dcad1f59b641ad577f262cb4187a323
tami-pinheiro <tamirespinheiro@live.com>
2020-12-17 01:03:06 -0300

    adicionando as aulas 1, 2 e 3 de introdução ao JavaScript

---
+++ README.md
@@ -0,0 +1,9 @@
+Programação para internet com javaScript
+
+Na primeira aula o professor Rafael mostrou como criar a tag script dentro do arquivo html, mas no curso vamos criar a pasta js e depois o arquivo main.js, nesta aula também foi criadas algumas variáveis, alert e tratamento de variáveis.
+
+Na segunda aula foi necessário criar array de frutas e fazer testes com as variações da lista. 
+Também foi mostrado o jeito de dicionário da frutas (como fosse um objeto json).
+
+Na terceira aula foi explicado como é feito as estruturas, if, else, while, for e date.
+
---
+++ js/main.js
@@ -0,0 +1,99 @@
+//Primeira aula
+//var nome = "Tamires Pinheiro";
+//var idade = 30;
+//var idade2 = 10;
+//var frase = "chocolate é o doce mais gostoso"
+//alert("Bem vindo" + ", " + nome + "!" + "Você tem " + idade + " anos.");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+
+//console.log(frase.toLowerCase());letra minuscula
+
+//console.log(frase.toUpperCase());letra maiuscula 
+
+//console.log(frase.replace("chocolate", "pudim"));
+//alert(frase.replace("chocolate", "mousse"));
+
+
+//Segunda aula - Arrays
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva"); adicionar uva
+//lista.pop();elimina a laranja
+//console.log(lista);mostra todos os itens da lista
+
+//console.log(lista.length);mostra quantidade de itens da lista
+//console.log(lista.reverse()); mostrar a ordem ao contrario 
+
+//console.log(lista);
+//console.log(lista.join(" - "));mostrar string com - 
+//console.log(lista.toString());mostra no formato string
+
+
+//console.log(lista); imprimir todas as frutas
+//console.log(lista[1]);imprimir só a pêra
+//alert(lista[1]); só a pêra também
+
+//var fruta = {nome:"maça", cor:"vermelha"} dicionário da fruta maça
+//console.log(fruta.nome);mostrar só o nome da fruta
+//alert(fruta.cor);mostrar a cor da fruta
+
+//var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}] dicionário das frutas, parece um arquivo json
+//console.log(frutas);
+//alert(frutas[1].nome);mostrar o nome uva
+
+//Terceira aula - Condicionais
+
+//Estrutura se 
+/*
+var idade = prompt("Qual sua idade?"); vai perguntar a idade e vai guardar na variável idade
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+//Estrutura enquanto
+/*var count = 0
+while(count <= 5){
+    console.log(count);
+    //alert(count);caixa na tela mostrando numeros de 0 a 5
+    count = count + 1;
+};
+*/
+
+//Estrutura para
+/*var count;
+for(count = 0; count <= 5; count ++){
+   alert(count) 
+};
+*/
+
+//Estrutura data
+//var d = new Date();mostrar data atual completa
+//alert(d);
+
+//var d = new Date();mostrar mês, ex: estamos em dezembro, então mostrou 12
+//alert(d.getMonth()+1);
+
+//var d = new Date();mostrar minutos, ex: são 00:23, então mostrou só 23 minutos
+//alert(d.getMinutes());
+
+//var d = new Date();mostrar a data
+//alert(d.getHours());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
